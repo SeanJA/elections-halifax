0fcd4197f196748ef6b1b439e3e66cbfb72baf9b
SeanJA <sean.sandy@gmail.com>
2012-09-14 00:57:55 -0300

    Added sharing buttons
    Also moved the thanks stuff to the right

--- index.php
+++ index.php
@@ -44,8 +44,30 @@
 				<div id="map-canvas"></div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="eight columns centered">
+				<hr />
+			</div>
+		</div>
+		<div class="row">
+			<div class="four columns centered">
+
+				<!-- AddThis Button BEGIN -->
+				<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+				<a class="addthis_button_preferred_1"></a>
+				<a class="addthis_button_preferred_2"></a>
+				<a class="addthis_button_preferred_3"></a>
+				<a class="addthis_button_preferred_4"></a>
+				<a class="addthis_button_compact"></a>
+				<a class="addthis_counter addthis_bubble_style"></a>
+				</div>
+				<script type="text/javascript">var addthis_config = {"data_track_addressbar":false};</script>
+				<script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e6d6dff33a5a83e"></script>
+				<!-- AddThis Button END -->
+			</div>
+		</div>
 		<footer class="row">
-			<div class="twelve columns">
+			<div class="two columns offset-by-ten">
 				<ul>
 					<li>
 						<small>District data from <a target="_blank" href="http://www.openfile.ca/halifax/data/hrm-election-2012-district-profiles">OpenFile.ca</a></small>
