90ecea47f7df2ad187d7b7c0bbca70e0729fad69
SeanJA <sean.sandy@gmail.com>
2012-09-14 15:02:34 -0300

    Added a twitter feed
    Follows the #votehrm tag

--- index.php
+++ index.php
@@ -40,6 +40,12 @@
 				</h4>
 				<input type="text" value="b3h 1s3" name="postal-code" id="postal-code" />
 				<a id="find-it" class="large button">Find It!</a>
+				<div class="hide-for-small">
+					<a class="twitter-timeline" href="https://twitter.com/search?q=%23votehrm" data-widget-id="246648916957859840">
+					 	Vote HRM Tweets
+					</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
 			</div>
 			<div class="eight columns">
 				<div id="map-canvas" class="map"></div>
