79d056fb27499f86c1d2c691ba79b88837f6c8d2
SeanJA <sean.sandy@gmail.com>
2012-09-13 23:29:32 -0300

    index.php is the new index.html (also basic layout setup)

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+	<!--<![endif]-->
+	<head>
+		<meta charset="utf-8"><!-- Set the viewport width to device width for mobile -->
+		<meta name="viewport" content="width=device-width">
+		<title>
+			Welcome to Foundation
+		</title><!-- Included CSS Files -->
+		<link rel="stylesheet" href="stylesheets/app.css" type="text/css">
+		<script src="javascripts/foundation/modernizr.foundation.js" type="text/javascript">
+</script><!-- IE Fix for HTML5 Tags -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	</head>
+	<body>
+		<div class="row">
+			<div class="twelve columns">
+				<h2>
+					Find your new district
+				</h2>
+				<p>
+					Your district may have changed, find your new one.
+				</p>
+				<hr>
+			</div>
+		</div>
+		<div class="row">
+			<div class="four columns">
+				<h4>
+					Enter your postal code
+				</h4>
+				<input type="text" value="b3h 1s3" name="postal-code" id="postal-code" />
+			</div>
+			<div class="eight columns">
+				
+			</div>
+
+		</div><!-- Included JS Files (Uncompressed) -->
+		<script src="javascripts/foundation/jquery.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.accordion.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.alerts.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.buttons.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.forms.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.mediaQueryToggle.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.navigation.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.orbit.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.reveal.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.tabs.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.foundation.tooltips.js" type="text/javascript">
+</script><script src="javascripts/foundation/jquery.placeholder.js" type="text/javascript">
+</script><!-- Application Javascript, safe to override -->
+		<script src="javascripts/foundation/app.js" type="text/javascript">
+</script>
+	</body>
+</html>
