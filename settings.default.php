3746e665e7b0d0ee63873e845c6341894268a667
SeanJA <sean.sandy@gmail.com>
2012-09-14 23:54:35 -0300

    added the json arrays to the settings file

--- settings.default.php
+++ settings.default.php
@@ -4,3 +4,4 @@ $sensor = 'true';
 $api_key = '';
 $google_analytics = '';
 $polling_stations = file_get_contents('districts/polling-stations.json');
+$districts = file_get_contents('districts/districts.json');
