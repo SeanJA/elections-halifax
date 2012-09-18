5bafc5573156148c7331e83e9fbf9458e77a5104
SeanJA <sean.sandy@gmail.com>
2012-09-18 17:19:51 -0300

    Added feedback linkage

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
 		<footer class="row">
 			<div class="ten columns">
 				<p>
-					Disclaimer: While I tried to get this stuff right, I makde no promise that I did. If you see a problem (or you have some of the links that I missed...), or you want something else on here... hit me up on <a target="_blank" href="http://twitter.com/SeanJA">@SeanJA</a>, or on app.net <a target="_blank" href="http://alpha.app.net">@SeanJA</a> I will see what I can do.
+					Disclaimer: While I tried to get this stuff right, I makde no promise that I did (I am a private citizen and not a major news organization, I don't have access to the latest data all the time). If you see a problem (or you have some of the links that I missed...), or you want something else on here... <span class="hide-for-small inline" style="display: inline;">you can use the feedback link on the site ,</span> hit me up on <a target="_blank" href="http://twitter.com/SeanJA">@SeanJA</a>, or on app.net <a target="_blank" href="http://alpha.app.net">@SeanJA</a> I will see what I can do.
 				</p>
 				<p>
 					Wanna see me elsewhere / see other things that I have done? <a target="_blank" href="http://seanja.info">Take a look over here.</a>
@@ -172,5 +172,23 @@
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
 		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+		<script type="text/javascript">
+		  var uvOptions = {};
+		  (function() {
+		    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+		    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/LEg0E80atR8y4piEgix37A.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+		  })();
+		</script>
+		<style>
+			@media only screen and (max-width: 767px){
+				#uvTabLabel{
+					display: none !important;
+				}
+			}
+			.inline {
+				display: inline !important;
+			}
+		</style>
 	</body>
 </html>
