2e88d0e14aca70cef96dafbd8fd145588f611d0d
SeanJA <sean.sandy@gmail.com>
2012-09-20 01:10:16 -0300

    Fancy blurred backgrounds added
    Moved the css that was in the index file out, had to add some more css
    there so that the background could alternate
    
    Added more to the thanks section

--- Readme.md
+++ Readme.md
@@ -12,6 +12,11 @@ Install foundation `[sudo] gem install zurb-foundation`
 
 copy `settings.default.php` to `settings.php` and fill it out with your info (you could just leave the values blank)
 
+
 ## Thanks
 
-Open file for the district data
+Open File Halifax: for the district data
+
+Josh Bousel for the image:   http://www.flickr.com/photos/joshbousel/36548458/
+
+Dennis Jarvis for the images: http://www.flickr.com/photos/archer10/2261846148/ and http://www.flickr.com/photos/archer10/6812321707/
--- index.php
+++ index.php
@@ -51,8 +51,8 @@
 			</div>
 		</header>
 		<div class="row hide-for-small">
-			<div class="eight columns centered">
-				<ul class="link-list">
+			<div class="seven columns centered">
+				<ul class="link-list no-margin">
 					<li>
 						<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://election.seanja.com" data-hashtags="votehrm" data-dnt="true">Tweet</a>
 					</li>
@@ -63,7 +63,7 @@
 						<div class="g-plus" data-action="share" data-annotation="bubble" data-height="15" data-href="http://election.seanja.com"></div>
 					</li>
 					<li>
-						<div class="fb-like" ref="top" data-href="http://election.seanja.com" data-send="true" data-width="212" data-show-faces="false"></div>
+						<div class="fb-like" ref="top" data-href="http://election.seanja.com" data-send="true" data-layout="button_count" data-width="212" data-show-faces="false"></div>
 					</li>
 				</ul>
 			</div>
@@ -94,8 +94,8 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="eight columns centered">
-				<ul class="link-list">
+			<div class="seven columns centered">
+				<ul class="link-list no-margin">
 					<li>
 						<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://election.seanja.com" data-hashtags="votehrm" data-dnt="true">Tweet</a>
 					</li>
@@ -106,15 +106,15 @@
 						<div class="g-plus" data-action="share" data-annotation="bubble" data-height="15" data-href="http://election.seanja.com"></div>
 					</li>
 					<li>
-						<div class="fb-like" ref="bottom" data-href="http://election.seanja.com" data-send="true" data-width="212" data-show-faces="false"></div>
+						<div class="fb-like" ref="bottom" data-href="http://election.seanja.com" data-send="true" data-layout="button_count" data-width="212" data-show-faces="false"></div>
 					</li>
 				</ul>
 			</div>
 		</div>
 		<footer class="row">
-			<div class="ten columns">
+			<div class="ten columns panel">
 				<p>
-					Disclaimer: While I tried to get this stuff right, I makde no promise that I did (I am a private citizen and not a major news organization, I don't have access to the latest data all the time). If you see a problem (or you have some of the links that I missed...), or you want something else on here... <span class="hide-for-small inline" style="display: inline;">you can use the feedback link on the site,</span> hit me up on twitter <a target="_blank" href="http://twitter.com/SeanJA">@SeanJA</a>, on app.net <a target="_blank" href="http://alpha.app.net">@SeanJA</a>, create an issue in <a target="_blank" href="https://github.com/SeanJA/elections-halifax">github</a> or send me a support email at <a href="mailto:tickets@hrmelection.uservoice.com">tickets@hrmelection.uservoice.com</a>. I will see what I can do.
+					Disclaimer: While I tried to get this stuff right, I makde no promise that I did (I am a private citizen and not a major news organization, I don't have access to the latest data all the time). If you see a problem (or you have some of the links that I missed...), or you want something else on here... <span class="hide-for-small inline" style="display: inline;">you can use the feedback link on the right side of the site,</span> hit me up on twitter <a target="_blank" href="http://twitter.com/SeanJA">@SeanJA</a>, on app.net <a target="_blank" href="http://alpha.app.net">@SeanJA</a>, create an issue in <a target="_blank" href="https://github.com/SeanJA/elections-halifax">github</a> or send me a support email at <a href="mailto:tickets@hrmelection.uservoice.com">tickets@hrmelection.uservoice.com</a>. I will see what I can do.
 				</p>
 				<p>
 					Wanna see me elsewhere / see other things that I have done? <a target="_blank" href="http://seanja.info">Take a look over here.</a>
@@ -180,15 +180,27 @@
 		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
 		  })();
 		</script>
+
+		<?php $bg = rand(1,3); ?>
+
 		<style>
-			@media only screen and (max-width: 767px){
-				#uvTabLabel{
-					display: none !important;
+			/* some final overrides for a fancy changing background */
+			@media only screen and (min-width: 767px){
+				body{
+					background: url(images/bg<?php echo $bg; ?>.jpg) no-repeat center center fixed;
+					<?php if($bg === 2): ?>
+					color: #fff;
+					<?php endif; ?>
 				}
-			}
-			.inline {
-				display: inline !important;
+				<?php if($bg === 2): ?>
+					.panel{
+						background-color: #fff;
+					    border-color: #fff;
+					    color: #000;
+					}
+				<?php endif; ?>
 			}
 		</style>
+
 	</body>
 </html>
