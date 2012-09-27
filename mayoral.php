7e2783d49d0b69e3547946a2911303c5012dfe17
SeanJA <sean.sandy@gmail.com>
2012-09-27 08:14:57 -0300

    Missed one of the candidates, also alphabetic order

--- mayoral.php
+++ mayoral.php
@@ -37,31 +37,30 @@ include 'templates/header.php'; ?>
 			</div>
 
 			<div class="row">
+
 				<div class="three columns">
-					<img src="images/mike-head.png" alt="" />
+					<img src="images/aaron-head.png" alt="" />
 				</div>
 
 				<div class="one columns"></div>
 
 				<div class="four columns">
 					<div class="panel">
-						<h3>Mike Savage</h3>
-						<a href="http://mikesavage.ca" target="_blank">
-							<img src="images/mike.png" alt="" />
+						<h3>Aaron Eisses</h3>
+						<a href="http://aaroneisses.com/" target="_blank">
+							<img src="images/aaron.png" alt="" />
 						</a>
 						<div>
-							<a class="fc-webicon rss" href="http://mikesavage.ca" target="_blank">mikesavage.ca</a>
-							<a class="fc-webicon twitter" href="http://twitter.com/MikeSavageHRM" target="_blank">@MikeSavageHRM</a>
-							<a class="fc-webicon facebook" href="http://www.facebook.com/Ilikemikesavage" target="_blank">Ilikemikesavage</a>
-							<a class="fc-webicon mail" target="_blank" href="mailto:information@mikesavage.ca">information@mikesavage.ca</a>
+							<a class="fc-webicon rss" href="http://aaroneisses.com/" target="_blank">aaroneisses.com</a>
+							<a class="fc-webicon twitter" href="http://twitter.com/aaron_eisses" target="_blank">@aaron_eisses</a>
+							<a class="fc-webicon mail" target="_blank" href="mailto:aaroneisseshrm2012@gmail.com">aaroneisseshrm2012@gmail.com</a>
 						</div>
 					</div>
 				</div>
 
 				<div class="four columns">
-					<a class="twitter-timeline" data-dnt=true href="https://twitter.com/search?q=%40MikeSavageHRM" data-widget-id="249529939819380736" data-related="MikeSavageHRM,seanja">Tweets about "@MikeSavageHRM"</a>
+					<a class="twitter-timeline"  href="https://twitter.com/search?q=%40aaron_eisses" data-widget-id="250928851901878272">Tweets about "@aaron_eisses"</a>
 				</div>
-				
 			</div>
 
 			<div class="row">
@@ -93,6 +92,38 @@ include 'templates/header.php'; ?>
 
 				<div class="four columns">
 					<a class="twitter-timeline" data-dnt=true href="https://twitter.com/search?q=%40tomaskformore" data-widget-id="249530144702726144" data-related="tomaskformore,seanja">Tweets about "@tomaskformore"</a>
+				</div>
+				
+			</div>
+
+			<div class="row">
+				<hr />
+			</div>
+
+			<div class="row">
+				<div class="three columns">
+					<img src="images/mike-head.png" alt="" />
+				</div>
+
+				<div class="one columns"></div>
+
+				<div class="four columns">
+					<div class="panel">
+						<h3>Mike Savage</h3>
+						<a href="http://mikesavage.ca" target="_blank">
+							<img src="images/mike.png" alt="" />
+						</a>
+						<div>
+							<a class="fc-webicon rss" href="http://mikesavage.ca" target="_blank">mikesavage.ca</a>
+							<a class="fc-webicon twitter" href="http://twitter.com/MikeSavageHRM" target="_blank">@MikeSavageHRM</a>
+							<a class="fc-webicon facebook" href="http://www.facebook.com/Ilikemikesavage" target="_blank">Ilikemikesavage</a>
+							<a class="fc-webicon mail" target="_blank" href="mailto:information@mikesavage.ca">information@mikesavage.ca</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="four columns">
+					<a class="twitter-timeline" data-dnt=true href="https://twitter.com/search?q=%40MikeSavageHRM" data-widget-id="249529939819380736" data-related="MikeSavageHRM,seanja">Tweets about "@MikeSavageHRM"</a>
 				</div>
 				
 			</div>
